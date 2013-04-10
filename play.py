635dff28a1e8f4372f57437cd344c5805e79d873
Michel <lavoie.michel@gmail.com>
2013-04-10 16:34:54 -0300

    Update play.py

--- play.py
+++ play.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+# Copyright 2011-2012 Michel Lavoie
+
+# This file is part of Odb.
+
+# Odb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Odb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Odb. If not, see <http://www.gnu.org/licenses/>.
+
 import sys, select, mad, ao#, alsaaudio
 
 def isData():
