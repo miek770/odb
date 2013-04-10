676b2e1811ad5e4b1f038401a5e1b561db9b2e79
Michel <lavoie.michel@gmail.com>
2013-04-10 16:35:22 -0300

    Update vfd.py

--- vfd.py
+++ vfd.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-#=======================================================================
-# Module :      vfd.py
-# Auteur :      Michel Lavoie
-# Date :        21 juin 2012
-# Description : Module de contrôle de l'écran VFD2041 par le port série
-#               de l'ordinateur.
-#=======================================================================
+# Copyright 2011-2012 Michel Lavoie
+
+# This file is part of Odb.
+
+# Odb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Odb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Odb. If not, see <http://www.gnu.org/licenses/>.
 
 import serial
 
